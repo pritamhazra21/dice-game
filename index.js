a31f4e4df18365bc5599e3c079fc6404b9508a11
pritamhazra21 <pritamhazra.hello@gmail.com>
2022-06-18 16:16:50 +0530

    adding js

---
+++ index.js
@@ -0,0 +1,9 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
