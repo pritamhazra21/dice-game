1e728753d57863c46f2b752bbd679ea206c3b6ab
pritamhazra21 <pritamhazra.hello@gmail.com>
2022-06-18 16:22:42 +0530

    edi js

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
 
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = " 🚩 Player 1 wins!!";
+
+}else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 wins!!  🚩 ";
+
+}else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
